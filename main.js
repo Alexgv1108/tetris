d4bf461c98da36e7f6885bf03d4258e751743972
John Alexander Gallego Vasquez <alexgallego1108@gmail.com>
2023-09-20 23:10:32 -0500

    Add files via upload
    Se agrega evento de rotación del item

--- main.js
+++ main.js
@@ -8,9 +8,14 @@ const tableroAttr = tablero.getBoundingClientRect();
 const NUM_COLUMNAS = 14;
 const NUM_FILAS = 21;
 
+const PIXEL_VISIBLE = 'pixel visible fila-pieza';
+const PIXEL_INVISIBLE = 'pixel invisible fila-pieza';
+
 let idPieza = 1;
 let tamItemX = 0, tamItemY = 0, inicioX = 0, inicioY = 0, finalY = 0;
 let scrollX = 0;
+let piezaActual = [];
+let startX = 0;
 
 const botonInteraccion = document.getElementById('boton-interaccion');
 
@@ -21,19 +26,61 @@ const consultarPiezas = async () => {
 document.addEventListener('keydown', function(event) {
     const pieza = document.getElementById(idPieza);
     const piezaAttr = pieza.getBoundingClientRect();
+
+    // Mover hacia la derecha
     if (event.key === 'ArrowRight' || event.key === 'D' || event.key === 'd') {
         if (parseInt(tableroAttr.left + tableroAttr.width) > parseInt(piezaAttr.left + piezaAttr.width)) {
             pieza.style.left = (piezaAttr.x+tamItemX)+'px';
         }
-    } else if (event.key === 'ArrowLeft' || event.key === 'A' || event.key === 'a') {
+    } 
+    // Mover hacia la izquierda
+    else if (event.key === 'ArrowLeft' || event.key === 'A' || event.key === 'a') {
         if (parseInt(inicioX) < parseInt(piezaAttr.x)) {
             pieza.style.left = (piezaAttr.x-tamItemX)+'px';
         }
-    } else if (event.key === 'ArrowDown' || event.key === 'S' || event.key === 's') {
-        debugger;
+    } 
+    // Mover hacia abajo
+    else if (event.key === 'ArrowDown' || event.key === 'S' || event.key === 's') {
         if (parseInt(finalY) > parseInt(piezaAttr.y+piezaAttr.height+(piezaAttr.height/2))) {
             pieza.style.top = `${piezaAttr.top+tamItemY}px`;
         }
+    }
+    // Rotar pieza hacia la derecha
+    else if (event.key === 'e' || event.key === 'E') {
+        pieza.innerHTML = '';
+        const cantidadElementosX = piezaActual.length;
+        const cantidadElementosY = piezaActual[0].length;
+
+        const piezasInvertidas = [];
+
+        // rotación del elemento
+        for (let i = cantidadElementosY-1; i > -1 ; i--) {
+            const pixelAux = [];
+            for (let j = 0; j < cantidadElementosX; j++) {
+                pixelAux.push(piezaActual[j][i]);
+            }
+            piezasInvertidas.push(pixelAux);
+        }
+        crearPieza(pieza, piezasInvertidas);
+    }
+    // Rotar pieza hacia la izquierda
+    else if (event.key === 'q' || event.key === 'Q') {
+        pieza.innerHTML = '';
+        const cantidadElementosX = piezaActual.length;
+        const cantidadElementosY = piezaActual[0].length;
+
+        const piezasInvertidas = [];
+
+        // rotación del elemento
+        debugger;
+        for (let i = 0; i < cantidadElementosY ; i++) {
+            const pixelAux = [];
+            for (let j = cantidadElementosX-1; j > -1; j--) {
+                pixelAux.push(piezaActual[j][i]);
+            }
+            piezasInvertidas.push(pixelAux);
+        }
+        crearPieza(pieza, piezasInvertidas);
     }
 });
 
@@ -42,37 +89,51 @@ document.addEventListener('touchstart', function(event) {
     startX = event.touches[0].clientX;
 });
 
+document.addEventListener('dblclick', () => {
+    const pieza = document.getElementById(idPieza);
+    pieza.innerHTML = '';
+    const cantidadElementosX = piezaActual.length;
+    const cantidadElementosY = piezaActual[0].length;
+
+    const piezasInvertidas = [];
+
+    // rotación del elemento
+    for (let i = cantidadElementosY-1; i > -1 ; i--) {
+        const pixelAux = [];
+        for (let j = 0; j < cantidadElementosX; j++) {
+            pixelAux.push(piezaActual[j][i]);
+        }
+        piezasInvertidas.push(pixelAux);
+    }
+    crearPieza(pieza, piezasInvertidas);
+});
+
 document.addEventListener('touchmove', function(event) {
     const scrollX = event.touches[0].clientX - startX;
     const sensibilidad = 100;
 
     const pieza = document.getElementById(idPieza);
     const piezaAttr = pieza.getBoundingClientRect();
-  
-    debugger;
+
     if (scrollX > sensibilidad) {
         if (parseInt(tableroAttr.left + tableroAttr.width) > parseInt(piezaAttr.left + piezaAttr.width)) {
             pieza.style.left = (piezaAttr.x+tamItemX)+'px';
         }
-    } else if (scrollX < -sensibilidad) {
+    } 
+    // Movimiento hacia la izquierda
+    else if (scrollX < -sensibilidad) {
         if (parseInt(inicioX) < parseInt(piezaAttr.x)) {
             pieza.style.left = (piezaAttr.x-tamItemX)+'px';
         }
     }
 });
 
-const controllerTetris = () => {
-    const pieza = document.createElement("div");
-    pieza.className = 'pieza';
-    pieza.id = idPieza;
-    pieza.style.top = `${inicioY}px`;
-    debugger;
-    pieza.style.left = `${tableroAttr.left+(NUM_COLUMNAS/2-1)*tamItemX}px`;
-    tablero.appendChild(pieza);
-
+const crearPieza = (pieza, arrPiezas) => {
+    piezaActual = [];
     let filaPieza = null;
-    for (let i = 0; i < props.piezas[props.contadorPieza].value.length; i++) {
-        for (let j = 0; j < props.piezas[props.contadorPieza].value[i].length; j++) {
+    for (let i = 0; i < arrPiezas.length; i++) {
+        let itemPieza = [];
+        for (let j = 0; j < arrPiezas[i].length; j++) {
             if (j == 0) {
                 filaPieza = document.createElement("div");
                 filaPieza.style.height = `${tamItemY}px`;
@@ -80,17 +141,31 @@ const controllerTetris = () => {
             }
 
             const pixel = document.createElement('div');
+            pixel.id = `${idPieza}-${i}-${j}`;
             pixel.style.width = `${tamItemX}px`;
             pixel.style.height = `100%`;
-            if (props.piezas[props.contadorPieza].value[i][j]) {
-                pixel.className = `pixel visible fila-pieza`;
+            if (arrPiezas[i][j]) {
+                pixel.className = PIXEL_VISIBLE;
                 pixel.style.backgroundColor = props.piezas[props.contadorPieza].background;
             } else {
-                pixel.className = `pixel invisible fila-pieza`;
+                pixel.className = PIXEL_INVISIBLE;
             }
+            itemPieza.push(arrPiezas[i][j])
             filaPieza.appendChild(pixel);
         }
+        piezaActual.push(itemPieza);
     }
+}
+
+const controllerTetris = () => {
+    const pieza = document.createElement("div");
+    pieza.className = 'pieza';
+    pieza.id = idPieza;
+    pieza.style.top = `${inicioY}px`;
+    pieza.style.left = `${tableroAttr.left+(NUM_COLUMNAS/2-1)*tamItemX}px`;
+    tablero.appendChild(pieza);
+
+    crearPieza(pieza, props.piezas[props.contadorPieza].value);
 
     setTimeout(() => {
         const intervalPieza = setInterval(() => {
@@ -104,7 +179,7 @@ const controllerTetris = () => {
                 controllerTetris();
             }
         }, 500);
-    }, 1200);
+    }, 800);
 }
 
 const stylesTablero = () => {
