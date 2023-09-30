f92d2981ca8aee34443933586075ac22b7f0aa0c
John Alexander Gallego Vasquez <alexgallego1108@gmail.com>
2023-09-30 18:13:32 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const props = {
 };
 
 const tablero = document.getElementById('canvas');
-const tableroAttr = tablero.getBoundingClientRect();
+let tableroAttr = null;
 
 const NUM_COLUMNAS = 14;
 const NUM_FILAS = 21;
@@ -17,40 +17,85 @@ let tamItemX = 0, tamItemY = 0, inicioX = 0, inicioY = 0, finalY = 0;
 let scrollX = 0;
 let piezaActual = [];
 let startX = 0;
+let piezaColisionPosicion = 1;
 
 const botonInteraccion = document.getElementById('boton-interaccion');
 
 // EVENTOS
-document.addEventListener('keydown', function(event) {
-    const pieza = document.getElementById(idPieza);
-    const piezaAttr = pieza.getBoundingClientRect();
-
-    // Mover hacia la derecha
-    if (event.key === 'ArrowRight' || event.key === 'D' || event.key === 'd') {
-        if (parseInt(tableroAttr.left + tableroAttr.width) > parseInt(piezaAttr.left + piezaAttr.width)) {
-            pieza.style.left = (piezaAttr.x+tamItemX)+'px';
+const initEventos = () => {
+    document.addEventListener('keydown', function(event) {
+        const pieza = document.getElementById(idPieza);
+        const piezaAttr = pieza.getBoundingClientRect();
+    
+        // Mover hacia la derecha
+        if (event.key === 'ArrowRight' || event.key === 'D' || event.key === 'd') {
+            if (parseInt(tableroAttr.left + tableroAttr.width) > parseInt(piezaAttr.left + piezaAttr.width)) {
+                pieza.style.left = (piezaAttr.x+tamItemX)+'px';
+            }
+        } 
+        // Mover hacia la izquierda
+        else if (event.key === 'ArrowLeft' || event.key === 'A' || event.key === 'a') {
+            if (parseInt(inicioX) < parseInt(piezaAttr.x)) {
+                pieza.style.left = (piezaAttr.x-tamItemX)+'px';
+            }
+        } 
+        // Mover hacia abajo
+        else if (event.key === 'ArrowDown' || event.key === 'S' || event.key === 's' && !colision(piezaAttr)) {
+            if (parseInt(finalY) > parseInt(piezaAttr.y+piezaAttr.height+(piezaAttr.height/2))) {
+                pieza.style.top = `${piezaAttr.top+tamItemY}px`;
+            }
         }
-    } 
-    // Mover hacia la izquierda
-    else if (event.key === 'ArrowLeft' || event.key === 'A' || event.key === 'a') {
-        if (parseInt(inicioX) < parseInt(piezaAttr.x)) {
-            pieza.style.left = (piezaAttr.x-tamItemX)+'px';
+        // Rotar pieza hacia la derecha
+        else if (event.key === 'e' || event.key === 'E') {
+            pieza.innerHTML = '';
+            const cantidadElementosX = piezaActual.length;
+            const cantidadElementosY = piezaActual[0].length;
+    
+            const piezasInvertidas = [];
+    
+            // rotación del elemento
+            for (let i = cantidadElementosY-1; i > -1 ; i--) {
+                const pixelAux = [];
+                for (let j = 0; j < cantidadElementosX; j++) {
+                    pixelAux.push(piezaActual[j][i]);
+                }
+                piezasInvertidas.push(pixelAux);
+            }
+            crearPieza(pieza, piezasInvertidas);
         }
-    } 
-    // Mover hacia abajo
-    else if (event.key === 'ArrowDown' || event.key === 'S' || event.key === 's' && !colision(piezaAttr)) {
-        if (parseInt(finalY) > parseInt(piezaAttr.y+piezaAttr.height+(piezaAttr.height/2))) {
-            pieza.style.top = `${piezaAttr.top+tamItemY}px`;
+        // Rotar pieza hacia la izquierda
+        else if (event.key === 'q' || event.key === 'Q') {
+            pieza.innerHTML = '';
+            const cantidadElementosX = piezaActual.length;
+            const cantidadElementosY = piezaActual[0].length;
+    
+            const piezasInvertidas = [];
+    
+            // rotación del elemento
+            for (let i = 0; i < cantidadElementosY ; i++) {
+                const pixelAux = [];
+                for (let j = cantidadElementosX-1; j > -1; j--) {
+                    pixelAux.push(piezaActual[j][i]);
+                }
+                piezasInvertidas.push(pixelAux);
+            }
+            crearPieza(pieza, piezasInvertidas);
         }
-    }
-    // Rotar pieza hacia la derecha
-    else if (event.key === 'e' || event.key === 'E') {
+    });
+    
+    document.addEventListener('touchstart', function(event) {
+        // Registra las coordenadas iniciales al tocar la pantalla
+        startX = event.touches[0].clientX;
+    });
+    
+    document.addEventListener('dblclick', () => {
+        const pieza = document.getElementById(idPieza);
         pieza.innerHTML = '';
         const cantidadElementosX = piezaActual.length;
         const cantidadElementosY = piezaActual[0].length;
-
+    
         const piezasInvertidas = [];
-
+    
         // rotación del elemento
         for (let i = cantidadElementosY-1; i > -1 ; i--) {
             const pixelAux = [];
@@ -60,70 +105,28 @@ document.addEventListener('keydown', function(event) {
             piezasInvertidas.push(pixelAux);
         }
         crearPieza(pieza, piezasInvertidas);
-    }
-    // Rotar pieza hacia la izquierda
-    else if (event.key === 'q' || event.key === 'Q') {
-        pieza.innerHTML = '';
-        const cantidadElementosX = piezaActual.length;
-        const cantidadElementosY = piezaActual[0].length;
-
-        const piezasInvertidas = [];
-
-        // rotación del elemento
-        for (let i = 0; i < cantidadElementosY ; i++) {
-            const pixelAux = [];
-            for (let j = cantidadElementosX-1; j > -1; j--) {
-                pixelAux.push(piezaActual[j][i]);
+    });
+    
+    document.addEventListener('touchmove', function(event) {
+        const scrollX = event.touches[0].clientX - startX;
+        const sensibilidad = 100;
+    
+        const pieza = document.getElementById(idPieza);
+        const piezaAttr = pieza.getBoundingClientRect();
+    
+        if (scrollX > sensibilidad) {
+            if (parseInt(tableroAttr.left + tableroAttr.width) > parseInt(piezaAttr.left + piezaAttr.width)) {
+                pieza.style.left = (piezaAttr.x+tamItemX)+'px';
             }
-            piezasInvertidas.push(pixelAux);
+        } 
+        // Movimiento hacia la izquierda
+        else if (scrollX < -sensibilidad) {
+            if (parseInt(inicioX) < parseInt(piezaAttr.x)) {
+                pieza.style.left = (piezaAttr.x-tamItemX)+'px';
+            }
         }
-        crearPieza(pieza, piezasInvertidas);
-    }
-});
-
-document.addEventListener('touchstart', function(event) {
-    // Registra las coordenadas iniciales al tocar la pantalla
-    startX = event.touches[0].clientX;
-});
-
-document.addEventListener('dblclick', () => {
-    const pieza = document.getElementById(idPieza);
-    pieza.innerHTML = '';
-    const cantidadElementosX = piezaActual.length;
-    const cantidadElementosY = piezaActual[0].length;
-
-    const piezasInvertidas = [];
-
-    // rotación del elemento
-    for (let i = cantidadElementosY-1; i > -1 ; i--) {
-        const pixelAux = [];
-        for (let j = 0; j < cantidadElementosX; j++) {
-            pixelAux.push(piezaActual[j][i]);
-        }
-        piezasInvertidas.push(pixelAux);
-    }
-    crearPieza(pieza, piezasInvertidas);
-});
-
-document.addEventListener('touchmove', function(event) {
-    const scrollX = event.touches[0].clientX - startX;
-    const sensibilidad = 100;
-
-    const pieza = document.getElementById(idPieza);
-    const piezaAttr = pieza.getBoundingClientRect();
-
-    if (scrollX > sensibilidad) {
-        if (parseInt(tableroAttr.left + tableroAttr.width) > parseInt(piezaAttr.left + piezaAttr.width)) {
-            pieza.style.left = (piezaAttr.x+tamItemX)+'px';
-        }
-    } 
-    // Movimiento hacia la izquierda
-    else if (scrollX < -sensibilidad) {
-        if (parseInt(inicioX) < parseInt(piezaAttr.x)) {
-            pieza.style.left = (piezaAttr.x-tamItemX)+'px';
-        }
-    }
-});
+    });
+}
 // FIN EVENTOS
 
 const consultarPiezas = async () => {
@@ -131,7 +134,6 @@ const consultarPiezas = async () => {
 }
 
 const crearPieza = (pieza, arrPiezas) => {
-    debugger;
     piezaActual = [];
     let filaPieza = null;
     for (let i = 0; i < arrPiezas.length; i++) {
@@ -170,6 +172,7 @@ const controllerTetris = () => {
 
     crearPieza(pieza, props.piezas[props.contadorPieza].value);
 
+    debugger;
     setTimeout(() => {
         const intervalPieza = setInterval(() => {
             const piezaAttr = pieza.getBoundingClientRect();
@@ -177,6 +180,7 @@ const controllerTetris = () => {
                 pieza.style.top = (piezaAttr.y+tamItemY)+'px';
             } else {
                 idPieza++;
+                piezaColisionPosicion = 1;
                 props.contadorPieza = props.contadorPieza < (props.piezas.length-1) ? ++props.contadorPieza : 0;
                 clearInterval(intervalPieza);
                 controllerTetris();
@@ -186,32 +190,52 @@ const controllerTetris = () => {
 }
 
 const colision = pieza => {
-    for (let i = 1; i < idPieza; i++) {
+    for (let i = piezaColisionPosicion; i < idPieza; i++) {
         const piezaColision = document.getElementById(i);
         const piezaColisionAttr = piezaColision.getBoundingClientRect();
-        if (parseInt(pieza.y+pieza.height) === parseInt(piezaColisionAttr.y) 
+        if (parseInt(pieza.y+pieza.height) >= parseInt(piezaColisionAttr.y)
+            && parseInt(pieza.y+pieza.height) <= parseInt(piezaColisionAttr.y+piezaColisionAttr.height)
             && parseInt(pieza.x+pieza.width) > parseInt(piezaColisionAttr.x)
             && parseInt(pieza.x) < parseInt(piezaColisionAttr.x+piezaColisionAttr.width)) {
-                debugger;
-                const validacionPixels = validarPixelColision(piezaColision, pieza);
-                if (!validacionPixels || validacionPixels === piezaActual[0].length) {
-                    return true;
-                }
+                const ultimaFilaPiezaActual = piezaActual[piezaActual.length-1];
+                const cantidadRegistrosValidos = ultimaFilaPiezaActual.filter(item => item).length; // modificar por el que es de pf
+                const contadorPixelsValidos = validarPixelColision(piezaColision, pieza, ultimaFilaPiezaActual);
+                piezaColisionPosicion = i;
+                return !contadorPixelsValidos || contadorPixelsValidos !== cantidadRegistrosValidos;
             }
     }
     return false;
 }
 
-const validarPixelColision = (piezaColision, pieza) => {
-    const ultimaFila = piezaColision.querySelectorAll("div.pixel");
-    let contador = 0;
-    for (let i = 0; i < (ultimaFila.length/2); i++) {
-        const pixelColision = ultimaFila[i].getBoundingClientRect();
-        if (ultimaFila[i].className === PIXEL_INVISIBLE) {
-            if (parseInt(pieza.x+(pieza.width*(contador))) === parseInt(pixelColision.x)) contador++; // falta validar cantidad de items
-        } 
+const validarPixelColision = (piezaColision, pieza, piezaActualUltimaPos) => {
+    const itemValidar = piezaColision.querySelectorAll("div.pixel");
+    const itemValidarXInit = parseInt(itemValidar[0].getBoundingClientRect().x);
+    const itemValidarXIFinal = parseInt(itemValidar[itemValidar.length-1].getBoundingClientRect().x+tamItemX);
+    let contadorPixelsValidos = 0;
+    let valueJ = 0;
+
+    for (let i = 0; i < piezaActualUltimaPos.length; i++) {
+        const posX = parseInt(pieza.x+(tamItemX*i));
+        if (posX < itemValidarXInit || parseInt(posX+tamItemX) > itemValidarXIFinal) {
+            continue;
+        }
+        if (!piezaActualUltimaPos[i]) {
+            contadorPixelsValidos++
+            continue;
+        }
+        for (let j = valueJ; j < itemValidar.length; j++) {
+            const pixelColision = itemValidar[j].getBoundingClientRect();
+            if (parseInt(pixelColision.y) === parseInt(pieza.y+pieza.height) && itemValidar[j].className === PIXEL_INVISIBLE) {
+                if (posX === parseInt(pixelColision.x) && posX === parseInt(pixelColision.x)) {
+                    contadorPixelsValidos++;
+                    valueJ = j+1;
+                    break;
+                }
+            } else valueJ = j;
+        }
     }
-    return contador;
+
+    return contadorPixelsValidos;
 }
 
 const stylesTablero = () => {
@@ -241,7 +265,7 @@ const audio = () => {
         color: '#716add',
         background: '#fff url(./assets/trees.png)',
         backdrop: `
-            rgba(0,0,123,0.4)
+            rgba(0,0,123,0.5)
             left top
             no-repeat
         `,
@@ -263,10 +287,21 @@ const audio = () => {
     });
 }
 
+const initCanvas = () => {
+    const altoPag = window.innerHeight;
+    const medidasAltoPagina = altoPag-(altoPag*0.15);
+    tablero.style.height = `${medidasAltoPagina}px`;
+    tablero.style.width = `${medidasAltoPagina/1.2}px`;
+    tablero.style.marginTop = `${altoPag*0.05}px`;
+    tableroAttr = tablero.getBoundingClientRect();
+}
+
 const init = async () => {
+    initCanvas();
     props.piezas = await consultarPiezas();
     stylesTablero();
     audio();
+    initEventos();
 }
 
 init();
